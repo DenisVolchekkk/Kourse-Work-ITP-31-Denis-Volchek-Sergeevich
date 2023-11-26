9563e8d9af4061d1047810da17acec43ad7957c6
Денис <denisvochek@mail.ru>
2023-11-26 19:19:04 +0300

    Kourse work

--- Univercity/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Univercity.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Univesaty.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Univesaty.Application
-{
-    //В проект Univesaty.Application добавить папки с классами
-    public class Class1
-    {
-
-    }
-}
